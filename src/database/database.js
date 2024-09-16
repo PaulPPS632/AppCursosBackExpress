8ca66d31c7cc03c3a24b30a205a676ddb310bbc1
Williams392 <williamsvalle392@gmail.com>
2024-09-15 23:10:58 -0500

    deploy railway

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize("app","root", "gBgcoUhrYxUynrTgloQljoMPdbsycNAX",{
-    host: "autorack.proxy.rlwy.net",
-    port: "35747",
+export const sequelize = new Sequelize("railway","root", "pKcoqhtJHeZgnkTWgSzIhFiptoisqMJz",{
+    host: "junction.proxy.rlwy.net",
+    port: "50255",
     dialect: "mysql"
 });
